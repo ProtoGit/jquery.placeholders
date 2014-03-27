50c6c15ce2950c29bffb69a7b22e6ac12cdc7d36
Adam Boxall <adam.boxall@gmail.com>
2014-03-27 11:19:27 +0000

    Initial commit

---
+++ jquery.placeholders.js
@@ -0,0 +1,46 @@
+(function($) {
+    $.fn.placeholders = function() {
+
+        var nativeSupport = (function() {
+            var input = document.createElement('input');
+            return ('placeholder' in input);
+        })();
+
+        this.each(function() {
+
+            var form = $(this);
+
+            form.find('input[title], textarea[title]').each(function() {
+                var field = $(this);
+                var title = field.attr('title');
+                if (nativeSupport) {
+                    field.attr('placeholder', title).removeAttr('title');
+                } else {
+                    if (field.val().length === 0) {
+                        field.addClass('placeholder').val(title).removeAttr('title');
+                    }
+                    field.on('focus', function() {
+                        if (field.val() === title) {
+                            field.val('');
+                            field.removeClass('placeholder');
+                        }
+                    });
+                    field.on('blur', function() {
+                        if (field.val() === '') {
+                            field.val(title);
+                            field.addClass('placeholder');
+                        }
+                    });
+                }
+            });
+
+            if (!nativeSupport) {
+                form.on('submit', function() {
+                    form.find('.placeholder').val('');
+                });
+            }
+
+        });
+
+    };
+})(jQuery);
